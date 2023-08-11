989b632ed993be291a40ab5b17de33e3171649e0
E L Anarcaya Roca <E.Anarcaya-Roca@sheffield.ac.uk>
2023-08-11 14:22:29 +0100

    app for prediction added

--- predict.py
+++ predict.py
@@ -6,22 +6,27 @@ import cv2
 model = YOLO('runs/detect/train/weights/best.pt')
 
 # specify the path to the folder containing the images
-folder_path = 'dataset/images/train'
+folder_path = 'dataset/images/train/'
 
 # get a list of all the files in the folder
 file_list = os.listdir(folder_path)
 
 # PRINT THE FIRST 10 IMAGES
-image = file_list[161]
+image = file_list[8]
+file_path = 'dataset/images/train/Part 7_A00756786_Res_3_Normals X.png'
 
 # Run inference on a single image
 # results = model('dataset/images/train/'+image, save=True)
 
 # Use the model to detect object - goat
-model.predict(source='dataset/images/train/'+image, save=False, show=True)
+result = model.predict(source=file_path, save=False, show=True)
+
 
 # keep the window open until we press a key
 cv2.waitKey(0)
+
+# close all windows
+cv2.destroyAllWindows()
 
 
 ### we can use the trained model to label pinsites
--- yoloApp.py
+++ yoloApp.py
@@ -1,61 +1,101 @@
-import os
 import tkinter as tk
-from PIL import Image, ImageTk
+from PIL import Image, ImageTk, ImageDraw
+import os
+import numpy as np
 from ultralytics import YOLO
 
-# specify the path to the folder containing the images
-folder_path = 'dataset/images/train'
+class App:
+    def __init__(self, master):
+        self.master = master
+        self.master.title("Image Viewer")
+        self.master.geometry("660x710")
 
-# get a list of all the files in the folder
-file_list = os.listdir(folder_path)
+        self.photo = None
 
-# filter out only the image files
-image_list = [file for file in file_list if file.endswith('.jpg') or file.endswith('.png')]
+        # Load a pretrained YOLO model (recommended for training)
+        self.model = YOLO('runs/detect/train/weights/best.pt')
 
-# create a YOLO model
-model = YOLO('yolov8n.pt')
+        # specify the path to the folder containing the images
+        self.folder_path = 'dataset/images/train'
 
-# create a Tkinter window
-window = tk.Tk()
+        # get a list of all the files in the folder
+        self.file_list = os.listdir(self.folder_path)
 
-# set the window title
-window.title('Image Viewer')
+        # set the current image index to 0
+        self.image_index = 0
 
-# set the window size
-window.geometry('800x600')
+        # create a canvas to display the image
+        self.canvas = tk.Canvas(self.master, width=640, height=640)
+        self.canvas.grid(row=0, column=0, padx=10, pady=10, sticky='we')
 
-# create a label to display the image
-image_label = tk.Label(window)
-image_label.pack()
+        # create a button to go to the next image
+        self.next_button = tk.Button(self.master, text="Next Image", command=self.next_image)
+        self.next_button.grid(row=1, column=0, padx=10, pady=10, sticky='we')
 
-# create a button to predict the next image
-def predict_next_image():
-    # get the next image in the list
-    global current_image_index
-    current_image_index += 1
-    if current_image_index >= len(image_list):
-        current_image_index = 0
-    image_path = os.path.join(folder_path, image_list[current_image_index])
-    
-    # run inference on the image
-    results = model(image_path)
-    
-    # display the image with bounding boxes and labels
-    image = Image.open(image_path)
-    image_with_boxes = results.render()
-    photo = ImageTk.PhotoImage(image_with_boxes)
-    image_label.config(image=photo)
-    image_label.image = photo
+        # display the first image
+        self.display_image()
 
-# create a button to predict the next image
-next_button = tk.Button(window, text='Predict Next', command=predict_next_image)
-next_button.pack()
+    def display_image(self):
+        # joint the folder path and the image name
+        image_path = os.path.join(self.folder_path, self.file_list[self.image_index])
 
-# initialize the current image index
-current_image_index = 0
+        # Run inference on a single image
+        results = self.model(image_path)
 
-# predict the first image
-predict_next_image()
+        # get the label, confidence and xyxy from the results
+        for r in results:
+            label = r.boxes.cls.cpu().numpy().astype(int)
+            conf = r.boxes.conf.cpu().numpy().astype(float)
+            xyxy = r.boxes.xyxy.cpu().numpy().astype(int)
 
-# start the Tkinter event loop
-window.mainloop()
+            # joint the label, confidence and xyxy into a single list of one dimension
+            label_conf_xyxy = np.concatenate((label.reshape(-1,1), conf.reshape(-1,1), xyxy), axis=1)
+
+        # get p1 and p2
+        p1 = label_conf_xyxy[:,2:4]
+        p2 = label_conf_xyxy[:,4:6]
+
+        # convert p1 and p2 to a list of tuples adn keep them as integers
+        p1 = [tuple(map(int, p)) for p in p1]
+        p2 = [tuple(map(int, p)) for p in p2]
+
+        # open the image and convert it to a tkinter PhotoImage
+        img = Image.open(image_path)
+        img = img.convert('RGB')
+
+        # draw the bounding boxes on the image
+        draw = ImageDraw.Draw(img)
+        for i in range(len(p1)):
+            draw.rectangle((p1[i], p2[i]), outline=(124,255,0), width=2)
+
+        # convert the image to a tkinter PhotoImage and add it to the canvas
+        self.photo = ImageTk.PhotoImage(img)
+
+        # create a white canvas the size of the image
+        self.canvas.create_image(0, 0, anchor='nw', image=self.photo)
+        
+        # update the image on the canvas
+        self.canvas.image = self.photo
+        self.canvas.grid(row=0, column=0, padx=10, pady=10, sticky='we')
+        
+        # create a button to go to the next image
+        self.next_button = tk.Button(self.master, text="Next Image", command=self.next_image)
+        self.next_button.grid(row=1, column=0, padx=10, pady=10, sticky='we')
+
+    def next_image(self):
+        # increment the image index
+        self.image_index += 1
+
+        # if we've reached the end of the file list, start over
+        if self.image_index == len(self.file_list):
+            self.image_index = 0
+
+        # clear the canvas
+        self.canvas.delete("all")
+
+        # display the next image
+        self.display_image()
+
+root = tk.Tk()
+app = App(root)
+root.mainloop()
